9b31c5f6c74580e451c9a1169c864391b0b72866
Durge Hidangmayum <durgehidang@gmail.com>
2024-06-06 22:40:31 +0530

    asdfsadf

--- src/cmp/blogpost.jsx
+++ src/cmp/blogpost.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 function BlogPost({ title, content, image }) {
     return (
         <article className="blog-post">
-            {image && <img src={image} alt={title} className="post-image" />} {/* Ensure correct class name is used */}
-            <div className="post-content">
+            {image && (
+                <div className="post-image-container">
+                    <img src={image} alt={title} className="post-image" />
+                </div>
+            )}
+            <div className="blog-post-content">
                 <h2>{title}</h2>
                 <p>{content}</p>
             </div>
