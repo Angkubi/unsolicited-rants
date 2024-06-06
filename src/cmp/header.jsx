c7f29b761238f550a01a97a681b43c6e18e69a3d
Durge Hidangmayum <durgehidang@gmail.com>
2024-06-06 10:07:14 +0530

    adfasdf

--- src/cmp/header.jsx
+++ src/cmp/header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-function Header() {
+const Header = () => {
+    const handleClick = () => {
+        // Redirect to the homepage
+        window.location.href = "/";
+    };
+
     return (
-        <header className="header">
+        <header className="header" onClick={handleClick} style={{ cursor: 'pointer' }}> {/* Add onClick event and cursor style */}
             <h1>UNSOLICITED RANTS</h1>
         </header>
     );
-}
+};
 
 export default Header;
-
