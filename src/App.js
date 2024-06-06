fd6ecdb59c12a867f25396fb56f5043309b237d1
Durge Hidangmayum <durgehidang@gmail.com>
2024-06-06 08:29:17 +0530

    asdf

--- src/App.js
+++ src/App.js
@@ -3,29 +3,20 @@ import Header from './cmp/header';
 import BlogList from './cmp/bloglist';
 import Footer from './cmp/footer';
 import './App.css';
-import img1 from './img/img1.jpg';
-import img2 from './img/img2.jpg';
-import img3 from './img/img3.jpg';
-import img4 from './img/img4.jpg';
-import img5 from './img/img5.jpg';
-import img6 from './img/img6.jpg';
-import img7 from './img/img7.jpg';
-import img8 from './img/img8.jpg';
-import img9 from './img/img9.jpg';
-import img10 from './img/img10.jpg';
+import images from './utils/imageloader';
 
 function App() {
   const [posts, setPosts] = useState([
-    { title: 'First ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img1 },
-    { title: 'Second ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img2 },
-    { title: 'Third ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img3 },
-    { title: 'Fourth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img4 },
-    { title: 'Fifth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img5 },
-    { title: 'Sixth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img6 },
-    { title: 'Seventh ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img7 },
-    { title: 'Eight ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img8 },
-    { title: 'Ninth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img9 },
-    { title: 'Tenth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: img10 },
+    { title: 'First ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img1.jpg'] },
+    { title: 'Second ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img2.jpg'] },
+    { title: 'Third ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img3.jpg'] },
+    { title: 'Fourth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img4.jpg'] },
+    { title: 'Fifth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img5.jpg'] },
+    { title: 'Sixth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img6.jpg'] },
+    { title: 'Seventh ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img7.jpg'] },
+    { title: 'Eight ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img8.jpg'] },
+    { title: 'Ninth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img9.jpg'] },
+    { title: 'Tenth ', content: 'This is a test. This is a test. This is a test. This is a test. This is a test. This is a test. This is a test.', image: images['img10.jpg'] },
   ]);
 
   return (
